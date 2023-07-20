aec2b900a627247286211bb63e9a8c962897bdf0
A_Spara_Guns <a.spara.guns@gmail.com>
2023-07-21 02:28:39 +0900

    コメントアウト解除

--- main.py
+++ main.py
@@ -6,24 +6,24 @@ from Writer import Writer
 
 path = "./states/"
 folderfile = os.listdir(path)
-# stateCSV = []
-# historyCSV = []
-# provinceCSV = []
-# pfkCSV = []
-# for fileName in folderfile:
-# 	filePath = path + fileName
-# 	code = Editor().loadFile(filePath)
+stateCSV = []
+historyCSV = []
+provinceCSV = []
+pfkCSV = []
+for fileName in folderfile:
+	filePath = path + fileName
+	code = Editor().loadFile(filePath)
 	
-# 	stateCSV += Setter(code).setStateData()
-# 	historyCSV += Setter(code).setHistoryData()
-# 	provinceCSV += Setter(code).setProvinceData()
-# 	pfkCSV += Setter(code).setStateArrayData()
+	stateCSV += Setter(code).setStateData()
+	historyCSV += Setter(code).setHistoryData()
+	provinceCSV += Setter(code).setProvinceData()
+	pfkCSV += Setter(code).setStateArrayData()
 	
-# Editor().writeCSV("state", Value().stateInfomation, stateCSV)
-# Editor().writeCSV("history", Value().historyInfomation, historyCSV)
-# Editor().writeCSV("province", Value().provinceInfomation, provinceCSV)
-# Editor().writeCSV("projectFKikan", Value().historyInfomation, pfkCSV)
-# Editor().defaultSortCSV()
+Editor().writeCSV("state", Value().stateInfomation, stateCSV)
+Editor().writeCSV("history", Value().historyInfomation, historyCSV)
+Editor().writeCSV("province", Value().provinceInfomation, provinceCSV)
+Editor().writeCSV("projectFKikan", Value().historyInfomation, pfkCSV)
+Editor().defaultSortCSV()
 
 for fileName in folderfile:
 	filePath = path + fileName
